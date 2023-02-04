63da78cce8b02de82c553cb73a39cc3a423d6923
zen <desu@boxxy>
2023-02-03 20:46:31 -0600

    modified:   merge_config.py

--- merge_config.py
+++ merge_config.py
@@ -17,18 +17,26 @@ DEFAULT_CONFIG_FILE = 'arch/x86/configs/x86_64_defconfig'
 DEFAULT_OUT_FILE = '.config'
 
 
+CONFIG_REGEX = regex.compile(r'(CONFIG)([a-zA-Z0-9_])+')
+_DEFAULT_OUT_FILE = DEFAULT_OUT_FILE
+_DEFINE_START = "CONFIG_"
+# Ensure slightly different rules for captures in quotes/not
+DEFINE_REGEX = r'^([a-zA-Z0-9_])+=(-?([a-zA-Z0-9])+|"([a-zA-Z0-9/_.,-=\(\) ])*")$'
+_UNDEFINE_START = "# CONFIG_"
+_UNDEFINE_END = " is not set"
+UNDEFINE_REGEX = regex.compile(r"^([a-zA-Z0-9_]+)$")
+_strict_fail = False
+
+
 class ConfigMerger:
     _CONFIG_REGEX = regex.compile(r'(CONFIG)([a-zA-Z0-9_])+')
     _DEFAULT_OUT_FILE = DEFAULT_OUT_FILE
-    _DEFINE_START = "CONFIG_"
     # Ensure slightly different rules for captures in quotes/not
     _DEFINE_REGEX = r'^([a-zA-Z0-9_])+=(-?([a-zA-Z0-9])+|"([a-zA-Z0-9/_.,-=\(\) ])*")$'
-    _UNDEFINE_START = "# CONFIG_"
-    _UNDEFINE_END = " is not set"
-    _UNDEFINE_REGEX = regex.compile(r"^([a-zA-Z0-9_]+)$")
+    _UNDEFINE_REGEX = regex.compile(r"^(# CONFIG_)([a-zA-Z0-9_]+)( is not set)$")
     _strict_fail = False
 
-    def __init__(self, base_file, merge_files, out_file_name=_DEFAULT_OUT_FILE, allnoconfig=False, strict_mode=False, log_level=logging.WARNING, no_make=False):
+    def __init__(self, base_file, merge_files, out_file_name=_DEFAULT_OUT_FILE, custom_parameters=[], allnoconfig=False, strict_mode=False, log_level=logging.WARNING, no_make=False):
         self.log_level = log_level
         self.logger = logging.getLogger("ConfigMerger")
         self.logger.setLevel(self.log_level)
@@ -43,6 +51,8 @@ class ConfigMerger:
         self.logger.debug("Set the base file name to: %s", self.base_file)
         self.merge_files = merge_files
         self.logger.debug("Set the merge files to: %s", self.merge_files)
+        self.custom_parameters = custom_parameters
+        self.logger.debug("Set the custom parameters to: %s", self.custom_parameters)
         self.out_file_name = out_file_name
         self.logger.debug("Set the output file name to: %s", self.out_file_name)
         self.allnoconfig = allnoconfig
@@ -52,162 +62,184 @@ class ConfigMerger:
         self.no_make = no_make
         self.logger.debug("Set no make to: %s", self.no_make)
 
-        self.load_config()
-        self.process_config()
+        self.process()
+
+    def process(self):
+        """
+        Processes the config based on the supplied parameters
+        """
+        # Load the base config
+        self.base_config = self.load_config()
+
+        # Merge config files
+        self.process_config_merge()
+
+        if self.custom_parameters:
+            for parameter in self.custom_parameters:
+                self.logger.debug("Attempting to parse passed config: %s", parameter)
+                name, value = self.parse_line(parameter)
+                self.logger.info("Loaded parameter from the command line: %s=%s", name, value)
+
         self.write_config()
 
-        if not no_make:
-            self.old_config = self.base_config.copy()
+        if not self.no_make:
             self.make_config()
-            self.base_file = out_file_name
-            self.logger.info("Changing the base file to: %s", self.base_file)
-            self.load_config()
-            self.compare_config()
+            make_processed_config = self.load_config(self.out_file_name)
+            self.compare_config(make_processed_config)
 
-    def line_to_config(self, file_line):
+    def classify_line(self, line):
         """
-        Attempts to convert a line in a .config file to a config parameter
-        Returns a tuple contaning the name of the parameter and a dict
-        containing the value information or if it should be removed from the config
-        and the parameter info, which is the line stripped of other information
+        Classifies a line as a define, undefine or unknown
+        Returns true for a define, false for an undefine
         """
-        # Do a basic string clean
-        line = file_line.rstrip()
         # Check that the line contains expected config syntax
-        if not regex.search(self._CONFIG_REGEX, line):
-            raise SyntaxWarning(f"`{line}` does not seem to be a kernel .config parameter")
-        # Define should be implied, gets unset by undefinitions
-        define = True
+        if not regex.search(CONFIG_REGEX, line):
+            raise SyntaxWarning(f"The following line does not seem to contain a kernel .config parameter: {line}")
 
-        # Ensure the entire line starts and ends with these values
-        if line.startswith(self._UNDEFINE_START) and line.endswith(self._UNDEFINE_END):
-            self.logger.debug("Detected an undefine")
-            # Removing all portions of the line other than the config variable name
-            config_var = line.replace(self._UNDEFINE_START, '').replace(self._UNDEFINE_END, '')
-            if regex.match(self._UNDEFINE_REGEX, config_var):
-                # Extract the regex match if it exists
-                config_var = regex.search(self._UNDEFINE_REGEX, config_var).group(1)
-                self.logger.debug("Detected config variable: %s", config_var)
-                # Return the config variable name, and instructions to not define
-                return config_var, {"define": False}
-            else:
-                raise ValueError(f"Configuration variable: {config_var} failed the regex")
-        # When a standard definition is detected
-        elif line.startswith(self._DEFINE_START):
-            self.logger.debug("Detected a defintion")
-            # Remove the start of the definition for processing
-            config_var = line.replace(self._DEFINE_START, '')
-            # Consider line comments
-            if "#" in config_var:
-                self.logger.debug("Comment detected, removing")
-                # Cut the string to where the comment is found
-                config_var = config_var[:config_var.find("#")]
-                # Clean the string
-                config_var = config_var.strip()
-            # Check that the string matches the definition regex
-            if regex.match(self._DEFINE_REGEX, config_var) or regex.match(self._DEFINE_REGEX, config_var):
-                # Find the location of the = character, to split the config_var and value
-                eq_loc = config_var.find('=')
-                # The value is everything after the equal sign
-                value = config_var[eq_loc + 1:]
-                self.logger.debug("Detected value: %s", value)
-                # The config_var is everything before, this also strips the = sign
-                config_var = config_var[:eq_loc]
-                if config_var and value:
-                    self.logger.debug("Detected config variable: %s", config_var)
-                    return config_var, {"define": define,
-                                        "value": value}
-                raise ValueError(f"Configuration variable is defined with no value: {config_var}")
-            else:
-                raise ValueError(f"Configuration variable faliled the regex: {config_var}")
+        if regex.match(self._DEFINE_REGEX, line):
+            return True
+
+        if regex.match(self._UNDEFINE_REGEX, line):
+            return False
+
+        raise SyntaxWarning(f"Unexpected config line: {line}")
+
+    def parse_line(self, input_line):
+        """
+        Attempts to parse an input line, returns a tuple and dict containing the config information
+        """
+        line = input_line.rstrip()
+        if self.classify_line(line):
+            return self.parse_define(line)
         else:
-            raise SyntaxWarning(f"Unable to parse possible config line: {line}")
+            return self.parse_undefine(line)
 
-    def compare_config(self):
+    def parse_define(self, input_line):
+        """
+        Attempts to parse a config definition, returns the name and a dict containing the values and whether it should be defined
+        Returns the name and value of the config line
+        """
+        line = self.strip_comment(input_line)
+        if not regex.match(self._DEFINE_REGEX, line):
+            raise SyntaxError(f"The input line failed the definition regex: {line}")
+
+        return self.split_parameter(input_line)
+
+    def strip_comment(self, input_line):
+        """
+        Removes comments from a string
+        """
+        if '#' in input_line:
+            self.logger.debug("Comment detected, removing: %s", input_line)
+            input_line = input_line[:input_line.find('#')].strip()
+            self.logger.debug("Processed line: %s", input_line)
+        return input_line
+
+    def split_parameter(self, input_config):
+        """
+        Splits a config line into a config variable and value
+        """
+        eq_loc = input_config.find('=')
+
+        name = input_config[:eq_loc]
+        self.logger.debug("Detected name: %s", name)
+
+        value = input_config[eq_loc + 1:]
+        self.logger.debug("Detected value: %s", value)
+
+        return name, value
+
+    def parse_undefine(self, input_line):
+        """
+        Attempts to parse a config undefinition
+        Returns the name of the config variable
+        """
+        if not regex.match(self._UNDEFINE_REGEX, input_line):
+            raise SyntaxError(f"The input line failed the undefinition regex: {input_line}")
+
+        self.logger.debug("Parsing line: %s", input_line)
+        name = "CONFIG_" + regex.search(self._UNDEFINE_REGEX, input_line).group(2)
+        self.logger.debug("Detected undefine for variable: %s", name)
+
+        return name, False
+
+    def compare_config(self, other_config):
         """
         Compare the key differences between then loaded config and old config
         Both objects should be the dict type used in this script
         """
-        for config_name in self.old_config:
+        for config_name in self.base_config:
             self.logger.debug("Checking config name: %s", config_name)
-            if config_name not in self.base_config:
+            if config_name not in other_config:
                 self.logger.warning("Argument `%s` is undefined when it should be set to: %s",
                                     config_name,
-                                    self.old_config[config_name].get('value'))
-            elif self.base_config[config_name] != self.old_config[config_name]:
+                                    self.base_config[config_name].get('value'))
+            elif other_config[config_name] != self.base_config[config_name]:
                 self.logger.warning("Argument value mismatch for: %s  | Found: %s | Expected: %s",
                                     config_name,
-                                    self.base_config[config_name].get('value'),
-                                    self.old_config[config_name].get('value'))
+                                    other_config[config_name].get('value'),
+                                    self.base_config[config_name].get('value'))
+            else:
+                self.logger.debug("Config check passed")
 
-    def load_config(self):
+    def load_config(self, config_file_name=None):
         """
         Loads self.base_file into self.base_config
         """
-        self.logger.debug("Loading the base config")
-        with open(self.base_file, 'r') as config_file:
+        if not config_file_name:
+            config_file_name = self.base_file
+
+        self.logger.debug("Loading the config file: %s", config_file_name)
+        with open(config_file_name, 'r') as config_file:
             kernel_config = {}
             self.logger.info("Processing the config file: %s", config_file.name)
             for line in config_file.readlines():
                 try:
-                    name, config = self.line_to_config(line)
-                    self.logger.debug("Detected config name: %s", name)
-                    # Ignore undefines in the base config
-                    if not config.get('define'):
-                        self.logger.debug("Line is an undefine in the base config, ignoring parameter: %s", name)
-                    else:
-                        self.logger.debug("Saving parameter: %s=%s", name, config.get('value'))
-                        kernel_config[name] = config
+                    name, value = self.parse_line(line)
+                    kernel_config[name] = value
                 # Allow the value errors but throw errors
-                except ValueError as e:
+                except SyntaxError as e:
                     self.logger.error(e)
                 except SyntaxWarning as e:
                     self.logger.debug(e)
             # Throw a value error if the file could not be processed
             if not kernel_config:
-                raise ValueError(f"Failed to load base config from {self.base_file}")
-        self.base_config = kernel_config
+                raise RuntimeWarning(f"Failed to load base config from {self.base_file}")
+        return kernel_config
 
-    def merge_config(self, merge_file_name):
+    def merge_config(self, merge_config):
         """
         Merges the supplied config wile with the base config
         If strict mode is enabled, errors will be emitted when parameters are redefined
         The script should process them all, but will eventually fail
         """
 
-        # Copy the dict
-        merged_config = self.base_config.copy()
-        self.logger.info("Opening config file for merge: %s", merge_file_name)
-        with open(merge_file_name, 'r') as merge_file:
-            for line in merge_file.readlines():
-                try:
-                    name, new_config = self.line_to_config(line)
-                    if name in self.base_config:
-                        self.logger.info("Config var aleady detected in the base config: %s=%s", name, self.base_config.get(name).get('value'))
-                        if self.strict_mode:
-                            self.logger.error("Attempting to redefine in strict mode: %s=%s", name, new_config.get('value'))
-                            self._strict_fail = True
-                        elif new_config.get('define'):
-                            self.logger.info("New value: %s", new_config.get('value'))
-                            merged_config[name] = new_config
-                        else:
-                            self.logger.info("Deleting config entry for config var: %s", name)
-                            del merged_config[name]
-                    else:
-                        if new_config.get('define'):
-                            self.logger.info("New config parameter set: %s=%s", name, new_config.get('value'))
-                            merged_config[name] = new_config
-                        else:
-                            self.logger.warning("Attempting to undefine a parameter which is not defined: %s", name)
-                except ValueError as e:
-                    self.logger.error(e)
-                except SyntaxWarning as e:
-                    self.logger.debug(e)
+        changed = False
 
-        if merged_config == self.base_config:
-            self.logger.warning("No changes detected after merging: %s", merge_file_name)
+        for name, value in merge_config.items():
+            if name in self.base_config:
+                self.logger.info("Config var aleady detected in the base config: %s=%s", name, self.base_config.get(name))
+                if self.strict_mode:
+                    self.logger.error("Attempting to redefine in strict mode: %s=%s", name, value)
+                    self._strict_fail = True
+                elif value:
+                    self.logger.info("New value: %s=%s", name, value)
+                    self.base_config[name] = value
+                    changed = True
+                else:
+                    self.logger.info("Deleting config entry for config var: %s", name)
+                    del self.base_config[name]
+                    changed = True
+            else:
+                if value:
+                    self.logger.info("New config parameter set: %s=%s", name, value)
+                    self.base_config[name] = value
+                    changed = True
+                else:
+                    self.logger.warning("Attempting to undefine a parameter which is not defined: %s", name)
 
-        self.base_config = merged_config
+        if not changed:
+            raise ValueError("No changes detected after processing config")
 
     def make_config(self):
         """
@@ -223,7 +255,7 @@ class ConfigMerger:
         if output != 0:
             raise RuntimeError(f"Unable to run make command, args: {make_args}")
 
-    def process_config(self):
+    def process_config_merge(self):
         """
         Merges
         Processes the base file into base config
@@ -232,14 +264,20 @@ class ConfigMerger:
         """
         # Load the base config from the passed base config file if it's not defined
         if not self.base_config:
-            self.load_config
+            self.logger.warning("Attempting to merge configs when no base config is loaded")
+            self.base_config = self.load_config()
 
         # Keep processing merge files while the list is populated
         # Item entries are popped from the list when processing has started
         # This should not run endlessly but may fail to process each merge section
         # Sections are applied over the base config as they are processed
         for merge_file in self.merge_files:
-            self.merge_config(merge_file)
+            try:
+                self.logger.info("Attempting to merge file: %s", merge_file)
+                self.merge_config(self.load_config(merge_file))
+            except RuntimeWarning as e:
+                self.logger.warning("%s file: %s", e, merge_file)
+
         self.logger.info("Merging has completed")
 
         if self._strict_fail:
@@ -254,9 +292,8 @@ class ConfigMerger:
         self.logger.info("Writing config file: %s", self.out_file_name)
         with open(self.out_file_name, 'w') as out_file:
             out_file.write("## Starting config\n")
-            for name, items in self.base_config.items():
-                value = items.get('value')
-                out_file.write(f"CONFIG_{name}={value}\n")
+            for name, value in self.base_config.items():
+                out_file.write(f"{name}={value}\n")
             out_file.write("## Ending config\n")
         self.logger.info("Wrote config file: %s", self.out_file_name)
 
@@ -307,6 +344,10 @@ if __name__ == '__main__':
     parser.add_argument('-s',
                         action='store_true',
                         help="Enable strict mode, the script will fail if any value is redefined")
+    # Add the parameter argument
+    parser.add_argument('-p',
+                        action='append',
+                        help="Specify parameters by command line")
     # First take the base argument
     # If this is the only argument, use it as the merge file using the DEFAULT_CONFIG_FILE as the base file
     parser.add_argument('base_file',
@@ -347,5 +388,12 @@ if __name__ == '__main__':
     for file in merge_files:
         logger.info("Considering file %s for merge", file)
 
-    config_merger = ConfigMerger(base_file, merge_files, out_file_name=args.o, allnoconfig=args.m, strict_mode=args.s, log_level=log_level, no_make=args.m)
+    config_merger = ConfigMerger(base_file,
+                                 merge_files,
+                                 custom_parameters=args.p,
+                                 out_file_name=args.o,
+                                 allnoconfig=args.m,
+                                 strict_mode=args.s,
+                                 log_level=log_level,
+                                 no_make=args.m)
 
